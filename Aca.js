a354787eb48e9fb1422a1b627c20f8d65b86b57f
Lolita Mkhitaryan <LolitaM@ogma.local>
2021-12-03 18:00:23 +0400

    mathmethods

--- Aca.js
+++ Aca.js
@@ -4,9 +4,51 @@ function getCount(num){
     }
     let count = 0;
     while(num > 0){
-        let lastDigit = num % 10;
-        num = (num - lastDigit) / 10;      
         count++;
+        num = Math.trunc(num / 10);      
+        
     }
-    console.log(count);
+    return count;
 }
+
+let value = getCount(7956);
+
+
+function detectPerson(age){
+    if(age < 0){
+        alert("invalid age")
+    } else {
+    if(age >= 18){
+        return 'adult';
+    } else if (age >= 2 && age < 18){
+        return 'child';
+    } else {
+        return 'infant';
+    }
+  }
+}
+
+let person = detectPerson(18);
+
+getSum(56, 68);
+function getSum(a, b){
+    return a + b;
+};
+
+let func = function(a, b){
+    return a + b;
+};
+
+Math.round(14.3)// округление
+Math.ceil(14.4)// округление вверх всегда
+Math.floor(-14.3)//  округление вниз всегда
+Math.trunc(14.4)// оставляет только целое число
+Math.random()
+Math.random() * 10
+Math.pow(2, 3)// 8
+Math.sqrt(9)// корень  из числа
+
+
+
+
+
